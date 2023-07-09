3105b64c2837b0432d602e5832f1cd6a4b6679e1
HsiaoCz <984274788@qq.com>
2023-07-09 16:40:14 +0800

    add something

--- api/server.go
+++ api/server.go
@@ -1,31 +1,38 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/HsiaoCz/ginknown/etc"
 	"github.com/HsiaoCz/ginknown/internal/service"
 	"github.com/HsiaoCz/ginknown/storage"
 	"github.com/gin-gonic/gin"
 )
 
 type Server struct {
-	r  *gin.Engine
-	uc *service.UserCase
+	addr string
+	port string
+	r    *gin.Engine
+	uc   *service.UserCase
 }
 
 func NewServer(r *gin.Engine, store *storage.Storage) *Server {
 	return &Server{
-		r:  r,
-		uc: service.NewUserCase(r, store),
+		addr: etc.Conf.AC.Addr,
+		port: etc.Conf.AC.Port,
+		r:    r,
+		uc:   service.NewUserCase(r, store),
 	}
 }
 
 func (s *Server) Start() error {
 	s.r.Use(gin.Recovery(), gin.Logger())
+	s.uc.RegisterRouter()
 	srv := http.Server{
 		Handler:      s.r,
-		Addr:         ":9091",
+		Addr:         fmt.Sprintf("%s:%s", s.addr, s.port),
 		WriteTimeout: 5 * time.Second,
 		ReadTimeout:  5 * time.Second,
 	}
